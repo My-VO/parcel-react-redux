ea9753f909808cacb078d77fbf21d2e1716619d2
My VO <kathy.nguyen198@gmail.com>
2021-05-17 15:14:54 +0200

    Add Redux with initialState

---
+++ src/js/store/root.js
@@ -0,0 +1,33 @@
+import {combineReducers} from 'redux';
+
+const initialState = {
+    name: "E-brairie",
+    init: false,
+    loading: false
+}
+
+const initialStateReducer = (state=initialState, action) => {
+    switch (action.type) {
+        case 'APP_INIT':
+            return {
+                ...state,
+                loading: true
+            }
+
+        case 'APP_READY':
+            return {
+                ...state,
+                init: true,
+                loading: false
+            }
+
+        case 'APP_RESET':
+            return state
+
+        default:
+            return state
+    }
+}
+const rootReducer = combineReducers({app: initialStateReducer})
+
+export default rootReducer
