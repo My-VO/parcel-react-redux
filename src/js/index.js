489a39a08f56f6326372dcfeee1ed7f82054ecc8
My VO <kathy.nguyen198@gmail.com>
2021-05-17 11:33:55 +0200

    Add React

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const MOUNT_NODE = document.getElementById('app');
+
+ReactDOM.render(
+    <div>
+        <h1>Hello !!!</h1>
+
+    </div>, MOUNT_NODE
+)
